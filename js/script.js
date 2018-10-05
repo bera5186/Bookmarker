14040eb236262cc0d6dea837b030f67fe6a1fee4
bera5186 <rbasu611@gmail.com>
2018-10-05 21:17:38 +0530

    Object creation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log('Script is connected');
+
+// Listen to the submit event
+document.getElementById('submit-btn').addEventListener("click", saveBookmark, false);
+
+// Function for listening to the event
+function saveBookmark(e){
+    console.log('its working');
+    // getting the values from the form element
+    var siteName = document.getElementById('siteName').value;
+    var siteURL  = document.getElementById('siteurl').value;
+
+    var bookMark = {
+        name : siteName,
+        url  : siteURL
+    }
+    console.log(bookMark)
+    
+    e.preventDefault();
+}
